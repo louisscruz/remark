5b801700437baf3de55d53ff8543c777a9ba1fe3
louisscruz <louisstephancruz@me.com>
2017-03-08 23:10:44 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Remark
+
+Remark reflows your markdown files.
---
+++ spec/remark-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import RemarkView from '../lib/remark-view';
+
+describe('RemarkView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
